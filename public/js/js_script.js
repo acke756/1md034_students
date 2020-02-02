2a7348a31f91bd7842bef958d1391bbe929e8fca
acke756 <acke756@gmail.com>
2020-02-02 12:42:27 +0100

    Created and used menuItem constructor.

---
+++ public/js/js_script.js
@@ -0,0 +1,23 @@
+function menuItem(name, kCal, hasGluten, hasLactose, hasPalmOil, imgSrc) {
+	this.name = name;
+	this.kCal = kCal;
+	this.hasGluten = hasGluten;
+	this.hasLactose = hasLactose;
+	this.hasPalmOil = hasPalmOil;
+	this.imgSrc = imgSrc;
+	this.info = function() {
+		return this.name + " " + this.kCal;
+	}
+}
+
+let stockyBurger = new menuItem("The Stocky Burger", 400, true, true, false, "https://image.shutterstock.com/image-photo/plant-based-burger-sesame-bun-600w-1543584050.jpg");
+let chickenDream = new menuItem("The Chicken Dream", 720, true, false, true, "https://image.shutterstock.com/image-photo/studio-shoot-delicious-chicken-burger-600w-1502962535.jpg");
+let hotternTottern = new menuItem("Hottern Tottern", 690, true, true, false, "https://image.shutterstock.com/image-photo/burger-600w-360983072.jpg");
+let vectorBurger = new menuItem("The Vector Burger", 420, false, true, false, "https://st2.depositphotos.com/6323354/10184/v/950/depositphotos_101843094-stock-illustration-big-tasty-burger-with-meat.jpg");
+let notBurger = new menuItem("Not Burger", 550, true, true, true, "https://media.gettyimages.com/photos/studio-shot-of-pepperoni-pizza-picture-id116360264?s=2048x2048");
+
+console.log(stockyBurger.info());
+console.log(chickenDream.info());
+console.log(hotternTottern.info());
+console.log(vectorBurger.info());
+console.log(notBurger.info());
