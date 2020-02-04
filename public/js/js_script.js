74cce49da8e919778c3a4c78760e5346c0ea9ffb
acke756 <acke756@gmail.com>
2020-02-04 17:34:26 +0100

    Made menu a json object instead.

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,3 +1,4 @@
+/*
 function menuItem(name, kCal, allergenes, imgSrc) {
 	this.name = name;
 	this.kCal = kCal;
@@ -12,7 +13,6 @@ menu.push(new menuItem("Hottern Tottern", 690, ["gluten", "lactose"], "https://i
 menu.push(new menuItem("The Vector Burger", 420, ["lactose"], "https://st2.depositphotos.com/6323354/10184/v/950/depositphotos_101843094-stock-illustration-big-tasty-burger-with-meat.jpg"));
 menu.push(new menuItem("Not Burger", 550, ["gluten", "lactose", "palm oil"], "https://media.gettyimages.com/photos/studio-shot-of-pepperoni-pizza-picture-id116360264?s=2048x2048"));
 
-/*
 let addEmptyNode = function(parentNode, nodeType) {
 	let newNode = document.createElement(nodeType);
 	parentNode.appendChild(newNode);
---
+++ public/js/menu.js
@@ -0,0 +1,47 @@
+let menu = [
+	{
+		"name": "The Stocky Burger",
+		"kCal": 400,
+		"allergenes": [
+			"gluten",
+			"lactose"
+		],
+		"imgSrc": "https://image.shutterstock.com/image-photo/plant-based-burger-sesame-bun-600w-1543584050.jpg"
+	},
+	{
+		"name": "The Chicken Dream",
+		"kCal": 720,
+		"allergenes": [
+			"gluten",
+			"palm oil"
+		],
+		"imgSrc": "https://image.shutterstock.com/image-photo/studio-shoot-delicious-chicken-burger-600w-1502962535.jpg"
+	},
+	{
+		"name": "Hottern Tottern",
+		"kCal": 690,
+		"allergenes": [
+			"gluten",
+			"lactose"
+		],
+		"imgSrc": "https://image.shutterstock.com/image-photo/burger-600w-360983072.jpg"
+	},
+	{
+		"name": "The Vector Burger",
+		"kCal": 420,
+		"allergenes": [
+			"lactose"
+		],
+		"imgSrc": "https://st2.depositphotos.com/6323354/10184/v/950/depositphotos_101843094-stock-illustration-big-tasty-burger-with-meat.jpg"
+	},
+	{
+		"name": "Not Burger",
+		"kCal": 550,
+		"allergenes": [
+			"gluten",
+			"lactose",
+			"palm oil"
+		],
+		"imgSrc": "https://media.gettyimages.com/photos/studio-shot-of-pepperoni-pizza-picture-id116360264?s=2048x2048"
+	}
+]
